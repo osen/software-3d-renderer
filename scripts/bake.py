00aa83bc056f0ba8dda35ec20b9ca19d39787d52
Karsten Pedersen <kpedersen@dae.local>
2019-03-19 18:41:41 +0000

    Updated bake script. Added transparent fence.

--- scripts/bake.py
+++ scripts/bake.py
@@ -4,62 +4,53 @@ def unselect_all():
 	for obj in bpy.data.objects:
 		obj.select = False
 
+	bpy.context.scene.objects.active = None
+
 def set_active(obj):
 	unselect_all()
 	obj.select = True
 	bpy.context.scene.objects.active = obj
 
-def load_obj(path):
-	unselect_all()
-	bpy.ops.import_scene.obj(filepath = path)
-
-	for obj in bpy.data.objects:
-		if obj.select == True:
-			unselect_all()
-			return obj
-
-	unselect_all()
-
-	return None
-
 def lightmap_unwrap(obj):
 	print("Generating light map [" + obj.name + "]")
 	set_active(obj)
-	bpy.ops.object.mode_set(mode = 'EDIT')
 
 	bakeMap = obj.data.uv_textures.new("BakeMap")
 	bakeMap.active = True;
 	image = bpy.data.images.new(obj.name + "_bake", width=2048, height=2048)
 	image.filepath = obj.name + "_bake.png"
 
+	for face in obj.data.polygons:
+		face.select = True
+
+	bpy.ops.object.mode_set(mode = 'EDIT')
 	bpy.data.screens['UV Editing'].areas[1].spaces[0].image = image
-
-	bpy.ops.uv.lightmap_pack(PREF_CONTEXT='ALL_FACES')
-
+	bpy.ops.uv.lightmap_pack()
 	bpy.ops.object.mode_set(mode = 'OBJECT')
+
+	for face in obj.data.polygons:
+		face.select = False
 
 	return image
 
 def strip_none(obj):
 	print("Removing textureless faces [" + obj.name + "]")
-
 	set_active(obj)
 	mesh = obj.data
+	selected = 0
 
 	for face in mesh.polygons:
 		material = mesh.materials[face.material_index]
 		if material.active_texture == None:
 			face.select = True
+			selected = selected + 1
 		else:
 			face.select = False
 
-	bpy.ops.object.mode_set(mode = 'EDIT')
-	bpy.ops.mesh.delete(type='FACE')
-	bpy.ops.object.mode_set(mode = 'OBJECT')
-	#bpy.ops.object.select_all(action='SELECT')
-
-	for face in mesh.polygons:
-		face.select = True
+	if selected > 0:
+		bpy.ops.object.mode_set(mode = 'EDIT')
+		bpy.ops.mesh.delete(type='FACE')
+		bpy.ops.object.mode_set(mode = 'OBJECT')
 
 def perform_bake(obj, image):
 	print("Performing bake [" + obj.name + "]")
@@ -68,35 +59,42 @@ def perform_bake(obj, image):
 		print("Nothing to bake [" + obj.name + "]")
 		return
 
+	#bpy.data.scenes["Scene"].render.bake_type = 'SHADOW'
 	bpy.ops.object.bake_image()
-	image.save()
+	#image.save()
+	image.save_render(image.filepath)
 
-outPath = bpy.path.display_name(bpy.context.blend_data.filepath)
-bpy.ops.object.mode_set(mode = 'OBJECT')
-bpy.ops.object.select_all(action='DESELECT')
+def bake_scene():
+	outPath = bpy.path.display_name_from_filepath(bpy.context.blend_data.filepath)
+	bpy.ops.object.mode_set(mode = 'OBJECT')
+	unselect_all()
 
-bpy.ops.export_scene.obj(
-	filepath=outPath + "_unlit.obj",
-	use_selection=False,
-	use_materials=True,
-	path_mode='RELATIVE')
+	for obj in bpy.data.objects:
+		if obj.type == "MESH":
+			obj.data.name = obj.name + "Mesh"
 
-for obj in bpy.data.objects:
-	if obj.type == "MESH":
-		unselect_all()
-		obj.select = True;
-		bpy.ops.object.delete()
+	bpy.ops.export_scene.obj(
+		filepath=outPath + ".obj",
+		path_mode='COPY')
 
-model = load_obj(outPath + "_unlit.obj")
+	for obj in bpy.data.objects:
+		if obj.type == "MESH":
+			obj.select = True;
+			bpy.ops.object.delete()
 
-for obj in bpy.data.objects:
-	if obj.type == "MESH":
-		strip_none(obj)
-		image = lightmap_unwrap(obj)
-		perform_bake(obj, image)
+	bpy.ops.import_scene.obj(filepath = outPath + ".obj")
 
-bpy.ops.export_scene.obj(
-	filepath=outPath + ".obj",
-	use_materials=True,
-	path_mode='RELATIVE')
+	for obj in bpy.data.objects:
+		if obj.type == "MESH":
+			strip_none(obj)
+
+			if obj.name != "NoBake_NoBakeMesh":
+				image = lightmap_unwrap(obj)
+				perform_bake(obj, image)
+
+	bpy.ops.export_scene.obj(
+		filepath=outPath + "_lightmap.obj",
+		path_mode='RELATIVE')
+
+bake_scene()
 
