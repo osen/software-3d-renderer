49b9d07d15baa2a60acd73ebe900e7b235fc36f4
Karsten Pedersen <kpedersen@local>
2019-06-07 21:05:47 +0100

    Added game lib.

--- src/net_platform/native/native.cpp
+++ src/net_platform/native/native.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
+extern "C"
+{
+
+void PlatformInit();
+
+}
+
 extern "C" __declspec(dllexport) void nativetest()
 {
   std::cout << "Hello" << std::endl;
+  PlatformInit();
 }
