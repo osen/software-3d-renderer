2efd6137e56502d73930f0910136d7a6d75599fc
Karsten Pedersen <kpedersen@local>
2019-06-07 18:41:28 +0100

    Initial creation of net_platform C# project.

---
+++ src/net_platform/net_platform/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace net_platform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
