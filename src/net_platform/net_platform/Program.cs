08ee1aa54f214042310a1e397e6b5bf689dd92ab
Karsten Pedersen <kpedersen@local>
2019-06-07 18:45:50 +0100

    Added winforms reference. Redirected build output. Note: I am doing many small commits so I can work out what the heck Visual Studio is doing every time I click a button ;)

--- src/net_platform/net_platform/Program.cs
+++ src/net_platform/net_platform/Program.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Windows.Forms;
 
-namespace net_platform
+class Program
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+  static void Main(string[] args)
+  {
+    Form form = new Form();
+    Application.Run(form);
+  }
 }
